246b54db8db2f79c1470c9b493e0894435ff7637
ROCKO360 <ricardo.jr.4@hotmail.com>
2017-04-21 18:24:11 -0600

    FINFIN
    FIN de pantallas

--- cookbook/resources/views/components/user.blade.php
+++ cookbook/resources/views/components/user.blade.php
@@ -1,7 +1,7 @@
 <div class="user">
     <div class="col-xs-12">
         <img src="../img/1.png" class="profile">
-        <span class="nombre"><a href="">{{$name}}</a></span>
+        <span class="nombre"><a href="/user/profile">{{$name}}</a></span>
         {{$typeButton}}
         @if(isset($removeButton))
         	{{$removeButton}}
